c5f12f4b0541bd294a37af0de1971989c0c9ccca
Elias Ortega <eliasortega2011@gmail.com>
2023-06-23 22:46:56 -0400

    enruteo

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const { leerTodo } = require('./services/productosService');
+const express = require('express');
+const clienteService = require('./services/clientesService');
 
-console.log(leerTodo('products'));
+const app = express();
+
+app.set('view engine', 'hbs');
+
+app.get('/clientes', (req, res) => {
+    res.render('clientes', {
+        titulo: 'Clientes',
+        arregloClientes: clienteService.leerTodo('customers')
+    });
+});
+
+app.get('/empleados', (req, res) => {
+    res.render('empleados', {
+        titulo: 'Empleados'
+    });
+});
+
+app.get('/detalleDeOrden', (req, res) => {
+    res.render('detalleDeOrden', {
+        titulo: 'Detalle de orden'
+    });
+});
+
+app.get('/ordenes', (req, res) => {
+    res.render('ordenes', {
+        titulo: 'Órdenes'
+    });
+});
+
+app.get('/productos', (req, res) => {
+    res.render('productos', {
+        titulo: 'Productos'
+    });
+});
+
+app.listen(8080);
